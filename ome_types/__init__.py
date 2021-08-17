21d75d9bc8297d2dac9d21cd9830b47fdcd90512
Talley Lambert <talley.lambert@gmail.com>
2021-08-17 10:39:48 -0400

    remove autogen stuff

--- ome_types/__init__.py
+++ ome_types/__init__.py
@@ -1,18 +1,13 @@
+from . import model
 from ._units import ureg
+from .model import OME
 
 try:
     from ._version import version as __version__
 except ImportError:
     __version__ = "unknown"
 
-try:
-    from . import model
-    from .model import OME
-except ModuleNotFoundError as e:
-    raise ModuleNotFoundError(
-        "Could not import 'ome_types.model.OME'.\nIf you are in a dev environment, "
-        "you may need to run 'python -m src.ome_autogen'" + str(e)
-    ) from None
+
 from .schema import to_dict, to_xml, validate  # isort:skip
 from ._convenience import from_tiff, from_xml  # isort:skip
 
